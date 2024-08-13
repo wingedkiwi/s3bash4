f5ae00d18ade9d38cb30e2985b423a35126240e9
Bailleux Benoît <benoit.bailleux@orange.com>
2024-08-13 17:30:03 +0200

    chore: Improve inline help and add proper copyright
    - Add the required option to prevent cUrl to display request progress
    - Add proper copyright and dev. mail address
    - Add license preamble to `lib/common.sh` file
    
    Signed-off-by: Bailleux Benoît <benoit.bailleux@orange.com>

--- lib/s3-common.sh
+++ lib/s3-common.sh
@@ -2,6 +2,19 @@
 #
 # Common functions for s3-bash4 commands
 # (c) 2015 Chi Vinh Le <cvl@winged.kiwi>
+# (c) 2024 Orange SA — author: benoit.bailleux@orange.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
 
 # Constants
 readonly VERSION="0.0.2"
@@ -247,7 +260,7 @@ performRequest() {
     payloadHash=$(sha256Hash "")
   fi
 
-  local cmd=("curl")
+  local cmd=("curl" "-sS") # Silent, but show errors
   local headers=
   local headerList=
 
